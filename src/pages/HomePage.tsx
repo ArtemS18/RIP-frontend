e9fa1b3cf74e9e712f1d650b5e69cc35fb6f6ba6
Artemii <rerttdtdf@gmail.com>
2025-11-17 19:45:27 +0300

    dix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,6 @@
-import { Button } from 'react-bootstrap';
 import './styles/HomePage.css';
-import { useNavigate } from 'react-router-dom';
 
 export const HomePage = () => {
-    const navigate = useNavigate();
-    const handleStart = (event: React.FormEvent) => {
-        navigate("/components"); 
-    };
     return (
         <div className="home-shell">
             <div className="home-inner">
@@ -16,10 +10,6 @@ export const HomePage = () => {
                 <p className="home-lead">
                     Этот сервис поможет DevOps инженерам расчитать доступность системы.
                 </p>
-                <Button className="btn add" type="button" onClick={handleStart}>
-                    Начать
-                </Button>
-
 
             </div>
         </div>
