54b02135caae33ab36218c787e0365223c138636
Artemii <rerttdtdf@gmail.com>
2025-11-17 21:00:07 +0300

    improve home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,18 @@
 import './styles/HomePage.css';
 import { Link } from 'react-router-dom';
 
-export const HomePage = () => {
-    return (
-        <div className="home-shell">
-            <div className="home-inner">
-                <Link to='/components'>
-                    <h1 className="page-title home-title-fix">
-                        Добро пожаловать в FailiverCheck!
-                    </h1>
-                    <p className="home-lead">
-                        Этот сервис поможет DevOps инженерам расчитать доступность системы.
-                    </p>
-                </Link>
-            </div>
-        </div>
-    );
-};
+export const HomePage = () => (
+  <div className="home-shell">
+    <div className="background-overlay"></div>
+    <div className="home-inner">
+      <Link to='/components'>
+        <h4 className="page-title home-title-animated">
+          Добро пожаловать в <br/> <span className="accent-word">FailiverCheck!</span>
+        </h4>
+        <p className="home-lead home-lead-animated">
+          Этот сервис поможет DevOps инженерам расчитать доступность системы.
+        </p>
+      </Link>
+    </div>
+  </div>
+);
