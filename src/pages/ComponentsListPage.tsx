826e5468a577235f4dfc7898a2564fa51e555e4f
Artemii <rerttdtdf@gmail.com>
2025-12-14 01:18:33 +0300

    fix

--- src/pages/ComponentsListPage.tsx
+++ src/pages/ComponentsListPage.tsx
@@ -91,7 +91,7 @@ export const ComonentsListPage = () => {
             {/* плавающая корзина */}
             <div className="systemCalc" title="Текущая заявка">
                 <Image
-                    src="https://localhost:3000/failivercheck/svg/bar-chat.svg"
+                    src="http://localhost:9000/failivercheck/svg/bar-chat.svg"
                     className="systemCalc__icon"
                     width={70}
                     height={70}
